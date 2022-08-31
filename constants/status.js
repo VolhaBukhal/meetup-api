10f6b1ae215cef550b93d7ffc2c93bfc90e5ef5b
volha <o.bukhal@gmail.com>
2022-08-31 08:29:52 +0300

    feat: add response statuses

---
+++ constants/status.js
@@ -0,0 +1,10 @@
+export const errorMessage =  'error'
+
+export const status = {
+  success: 200,
+  error: 500,
+  not_found: 404,
+  created: 201,
+  bad_request: 400,
+  no_content: 204,
+};
--- controllers/meetups.js
+++ controllers/meetups.js
@@ -1,30 +1,19 @@
-import {v4 as uuid} from 'uuid'
-
+import { v4 as uuid } from 'uuid'
 import { pool } from '../db.js'
-
-let meetups = [
-    {
-        title: "express discussion",
-        description: "need to get acquaintance",
-        tegs: ["node", "express", "backend"],
-        time: "2022-08-25",
-        place: "Victoria hotel",
-        id: "1"
-    },
-    {
-        title: "node.js discussion",
-        description: "need to get acquaintance",
-        tegs: ["node", "backend"],
-        time: "2022-08-26",
-        place: "Victoria hotel",
-        id: "2"
-    }
-]
+import { status, errorMessage } from '../constants/status.js'
 
 export const getMeetups =  async(req, res) => {
     const getAllMeetupsQuery = 'SELECT * FROM meetup'
-    const meetups = await pool.query(getAllMeetupsQuery)
-    res.json(meetups.rows)
+
+    try {
+        const meetups = await pool.query(getAllMeetupsQuery)
+        if (!meetups.rowCount) {
+            res.status(status.success).send('No meetups in database')
+        }
+        res.json(meetups.rows)
+    } catch (error) {
+        res.status(status.error).send(errorMessage);
+    }
 }
 
 export const createMeetup = async (req, res) => {
@@ -32,26 +21,42 @@ export const createMeetup = async (req, res) => {
     const {title, description, time, place} = req.body
     const createMeetupsQuery = 'INSERT INTO meetup (id_meetup, title, description, time, place) values($1, $2, $3, $4, $5) RETURNING *'
     const queryValues = [id, title, description, time, place]
-    const newMeetup = await pool.query(createMeetupsQuery, queryValues)
-    res.json(newMeetup.rows[0])
+    try {
+        const newMeetup = await pool.query(createMeetupsQuery, queryValues)
+        res.status(status.created).json(newMeetup.rows[0])
+    } catch (err) {
+        res.status(status.error).send(errorMessage)
+    }
 }
 
 export const getMeetupById =  async (req, res) => {
     const { id } = req.params;
     const getSingleMeetupQuery = 'SELECT * FROM meetup WHERE id_meetup = $1'
-    const singleMeetup = await pool.query(getSingleMeetupQuery, [id])
-    res.json(singleMeetup.rows[0])
+
+    try {
+        const singleMeetup = await pool.query(getSingleMeetupQuery, [id])
+        if (!singleMeetup.rowCount) {
+            res.status(status.not_found).send(`No meetup in database with id: ${id}`)
+        }
+        res.status(status.success).json(singleMeetup.rows[0])
+    }catch (err) {
+        res.status(status.error).send(errorMessage)
+    }
 }
 
 export const deleteMeetup = async (req, res) => {
     const { id } = req.params
     const deleteMeetupQuery = 'DELETE FROM meetup WHERE id_meetup = $1'
-    await pool.query(deleteMeetupQuery, [id], (err) => {
-        if (err) {
-            return console.error('Error executing query', err.stack)
+    try {
+        const responseDB = await pool.query(deleteMeetupQuery, [id])
+        if (!responseDB.rowCount) {
+            res.status(status.not_found).send(`Meetup with the id: ${id} doesn't exist in database`) 
+        } else {
+            res.status(status.success).send(`Meetup with the id: ${id} deleted from database`)
         }
-        res.send(`Meetup with the id: ${id} deleted from database`)
-    })
+    } catch (error) {
+        res.status(status.error).send(errorMessage)
+    }
 }
 
 export const updateMeetup = async (req, res) => {
@@ -60,8 +65,10 @@ export const updateMeetup = async (req, res) => {
 
     const updateMeetupQuery = 'UPDATE meetup SET title = COALESCE($1, title), description = COALESCE($2, description), time = COALESCE($3, time), place = COALESCE($4, place) WHERE id_meetup = $5 RETURNING *'
     const updateMeetupQueryValues = [title, description, time, place, id ]
-    
-    const updatedMeetup = await pool.query(updateMeetupQuery, updateMeetupQueryValues)
-    
-    res.json(updatedMeetup.rows[0])
+    try {
+        const updatedMeetup = await pool.query(updateMeetupQuery, updateMeetupQueryValues)
+        res.status(status.no_content).json('Meetup is updated')
+    } catch (error) {
+        res.status(status.error).send(errorMessage);
+    }
 }
