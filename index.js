fd22df33c3435fa917c8ebd5b71fea180a3423e3
volha <o.bukhal@gmail.com>
2022-09-05 13:37:35 +0300

    feat: add middleware swaggeri-ui-express

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-
+import fs from 'fs'
 import meetupsRoutes from './routes/meetups.js'
+import swaggerUi  from 'swagger-ui-express'
+const swaggerDocument = JSON.parse(fs.readFileSync('./openapi.json'))
 
 const app = express();
 
@@ -9,10 +11,11 @@ app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/meetups', meetupsRoutes)
+app.use( '/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World!!!'))
+app.get('/', (req, res) =>  res.json(swaggerDocument))
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}...`)
