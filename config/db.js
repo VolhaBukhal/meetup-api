a83765b7b8baaa807751d308c0ca9b414cec7e1f
volha <o.bukhal@gmail.com>
2022-10-05 15:54:52 +0300

    fix: updata config/db

--- config/db.js
+++ config/db.js
@@ -2,20 +2,23 @@ const pkg = require('pg')
 
 const { Pool } = pkg
 
-const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: 'meetups',
-}
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   database: 'meetups',
+// }
+
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
 }
 
-const pool = new Pool(
-  process.env.NODE_ENV === 'production' ? proConfig : devConfig
-)
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+})
 
 module.exports = { pool }
