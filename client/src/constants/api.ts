9e3a7f73179daf5aa37e399a7bc530d87d87d053
volha <o.bukhal@gmail.com>
2022-10-06 11:17:03 +0300

    fix: update baseurl on client with heroku link

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,2 +1,7 @@
 // export const baseUrl = 'http://localhost:5000'
-export const baseUrl = ''
+// export const baseUrl = ''
+
+export const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://meetup-test-m.herokuapp.com/'
+    : 'http://localhost:5000';
